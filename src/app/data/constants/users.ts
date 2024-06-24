e4ee770238b97e2a245ee81b9665842a7267ae1b
Guilherme Martins <gui.martins.soares@gmail.com>
2024-06-23 23:55:28 -0300

    Added template files for web page, components, and users. Removed default code

---
+++ src/app/data/constants/users.ts
@@ -0,0 +1,48 @@
+import { User } from "@/core/model/User"
+
+const users: User[] = [
+    {
+        id:  "1",
+        email: "ana@email.com.br",  
+        name:  "Ana",
+        password:  "123456",
+        active: true 
+    },
+    {
+        id:  "2",
+        email: "banana@email.com.br",  
+        name:  "Banana",
+        password:  "123456",
+        active: true 
+    },
+    {
+        id:  "3",
+        email: "cabana@email.com.br",  
+        name:  "Cabana",
+        password:  "123456",
+        active: true 
+    },
+    {
+        id:  "4",
+        email: "dana@email.com.br",  
+        name:  "Dana",
+        password:  "123456",
+        active: true 
+    },
+    {
+        id:  "5",
+        email: "evana@email.com.br",  
+        name:  "Evana",
+        password:  "123456",
+        active: true 
+    },
+    {
+        id:  "6",
+        email: "fhana@email.com.br",  
+        name:  "Fhana",
+        password:  "123456",
+        active: true 
+    },
+]
+
+export default users;
